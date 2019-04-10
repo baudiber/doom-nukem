08d7125e3f685e507c1e2f29f6f60cc3ad0815dc
Clement RICHARD <clrichar@e1r12p15.42.fr>
2019-04-10 15:03:34 +0200

    Correcting small issue

--- srcs/parser/parse_scan.c
+++ srcs/parser/parse_scan.c
@@ -6,7 +6,7 @@
 /*   By: clrichar <clrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 17:10:55 by clrichar          #+#    #+#             */
-/*   Updated: 2019/04/09 15:08:53 by clrichar         ###   ########.fr       */
+/*   Updated: 2019/04/10 15:02:46 by clrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static bool			check_data(char *line, t_data *data)
 	else if (!ft_strequ(line, "-\0")
 			&& data->max_x != (int)ft_countword(line, ' '))
 		ret = 1;
-	if (data->max_y > 64 || data->max_x > 64)
+	if (data->max_y > (324) || data->max_x > 64)
 		ret = 1;
 	while (ret == 0 && line[i])
 	{
@@ -86,7 +86,7 @@ void				parse_scan(t_data *data, char *map)
 	((fd = open(map, O_RDONLY)) < 0) ? parse_quit(data, 0, ERR_0) : 0;
 	scan_data(data, fd, NULL);
 	if (data->max_x < 1 || data->max_y < 1 || data->max_x > 64
-			|| data->max_y > 64 || !data->scan)
+			|| data->max_y > 324 || !data->scan)
 		parse_quit(data, 4, ERR_4);
 	if (!check_player(data))
 		parse_quit(data, 5, ERR_5);
