f2232ea42e4a6adcf837474d9e16541281a37307
Clement RICHARD <clrichar@e1r12p15.42.fr>
2019-04-10 14:39:19 +0200

    adding protection of define in editor

--- srcs/editor/main.c
+++ srcs/editor/main.c
@@ -6,7 +6,7 @@
 /*   By: roddavid <roddavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 14:41:40 by roddavid          #+#    #+#             */
-/*   Updated: 2019/04/09 16:45:39 by clrichar         ###   ########.fr       */
+/*   Updated: 2019/04/10 14:38:40 by clrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,42 @@ void		exit_error(int type)
 	exit(type);
 }
 
+static bool				basic_err(void)
+{
+	int					ret;
+
+	ret = 0;
+	if (FOV != 60)
+		ret = 1;
+	if (ret == 0 && (MAX_SIZE < 1 || MAX_MAPSIZE > 64))
+		ret = 1;
+	if (ret == 0 && margin != 40)
+		ret = 1;
+	if (ret == 0 && (WIN_W < 320 && WIN_W > 1920))
+		ret = 1;
+	if (ret == 0 && (WIN_H < 200 && WIN_H > 1280))
+		ret = 1;
+	if (ret == 0 && TIER != 5)
+		ret = 1;
+	if (ret == 0 && TILE_SIZE != 256)
+		ret = 1;
+	if (ret == 0 && (DWALL != 0 || DFLOOR != 1 || DSPRITE != 2 || DLIGHT != 3 \
+		|| DEVENT != 4))
+		ret = 1;
+	return (ret == 0) ? true : false;
+}
+
 int		main(int ac, char **av)
 {
 	t_e		e;
 	int		fd;
 
+	if (!basic_err())
+	{
+		ft_putendl("ERROR: Please do not modify define in header file");
+		return (0);
+	}
+	// a revoir
 	e.argc = ac;
 	e.argv = av;
 	fd = open(av[1], O_RDONLY);
