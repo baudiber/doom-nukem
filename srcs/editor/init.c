10d5d57f61a74699e07d1b5227562a72ff619aee
Clement RICHARD <clrichar@e1r12p15.42.fr>
2019-04-10 19:18:10 +0200

    Protecting some texture for the editor

--- srcs/editor/init.c
+++ srcs/editor/init.c
@@ -6,7 +6,7 @@
 /*   By: roddavid <roddavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 14:49:07 by roddavid          #+#    #+#             */
-/*   Updated: 2019/04/10 17:31:52 by roddavid         ###   ########.fr       */
+/*   Updated: 2019/04/10 19:17:22 by clrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,8 @@ void		init_draw_var(t_env *e)
 		e->draw[i].white.g = 255;
 		e->draw[i].white.b = 255;
 		e->draw[i].text = NULL;
-		e->draw[i].font = TTF_OpenFont("font/BEBAS.TTF", 160);
+		if (!(e->draw[i].font = TTF_OpenFont("font/BEBAS.TTF", 160)))
+			exit_error(2, ERR_2);
 	}
 }
 
