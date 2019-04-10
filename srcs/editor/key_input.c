dfa9f96a298c820c5b756605c4c9150604bf8c2e
Roderic <david.r72460@hotmail.com>
2019-04-10 20:18:24 +0200

    Save and quit by clicking

--- srcs/editor/key_input.c
+++ srcs/editor/key_input.c
@@ -6,7 +6,7 @@
 /*   By: roddavid <roddavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/06 17:37:38 by roddavid          #+#    #+#             */
-/*   Updated: 2019/04/10 15:23:30 by clrichar         ###   ########.fr       */
+/*   Updated: 2019/04/10 20:14:52 by roddavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,14 @@ void	mouse_click(t_env *e)
 		<= (WIN_W - MARGIN) && e->grid.mouseposy \
 		>= MARGIN && e->grid.mouseposy <= (WIN_H - MARGIN))
 		select_grid_block(e);
+	if (e->grid.mouseposx >= 40 && e->grid.mouseposx <= 180 \
+			&& e->grid.mouseposy >= 1160 && e->grid.mouseposy <= 1230 \
+			&& e->window.event.button.button == SDL_BUTTON_LEFT)
+		saving(e);
+	if (e->grid.mouseposx >= 260 && e->grid.mouseposx <= 400 \
+			&& e->grid.mouseposy >= 1160 && e->grid.mouseposy <= 1230 \
+			&& e->window.event.button.button == SDL_BUTTON_LEFT)
+		exit(0);
 }
 
 void	key_input(t_env *e)
