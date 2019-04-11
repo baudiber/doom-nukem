aa3a09db1b2791c44a810d7e7445d808ebc57950
Roderic <david.r72460@hotmail.com>
2019-04-11 19:36:58 +0200

    INGAME : Resized and functionnable menu

--- srcs_doom/menu/menu_options.c
+++ srcs_doom/menu/menu_options.c
@@ -6,7 +6,7 @@
 /*   By: roddavid <roddavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 18:56:03 by roddavid          #+#    #+#             */
-/*   Updated: 2019/03/29 17:45:08 by baudiber         ###   ########.fr       */
+/*   Updated: 2019/04/11 19:33:37 by roddavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void		mouse_menu_options(t_env *e, int x, int y, SDL_Event ev)
 		e->menu.save_img2 = e->menu.i_img;
 	e->menu.i_img = e->menu.save_img;
 	save = e->menu.i_img;
-	if ((x >= 390 && x <= 880) && (y >= 400 && y <= 450) \
+	if ((x >= 250 && x <= 280) && (y >= 300 && y <= 337) \
 	&& ev.button.button == SDL_BUTTON_LEFT && ev.button.state == SDL_PRESSED)
 		mouse_menu_options_2(e);
 	ft_slider(e, x, y, ev);
@@ -53,7 +53,7 @@ void		ft_menu_options_2(t_env *e, SDL_Event ev, int x, int y)
 		e->menu.i_img = 1;
 		e->menu.check_options = 1;
 	}
-	if ((x >= 1 && x <= 70) && (y >= 1 && y <= 60) \
+	if ((x >= 1 && x <= 50) && (y >= 1 && y <= 50) \
 		&& ev.button.button == SDL_BUTTON_LEFT)
 	{
 		e->menu.i_img = 1;
