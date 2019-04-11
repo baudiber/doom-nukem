ca7817f03d04b26125addb269fc4a3e750d852c5
Clement RICHARD <clrichar@e3r6p10.42.fr>
2019-04-11 18:46:03 +0200

    Adding some man at the begining

--- srcs_editor/main.c
+++ srcs_editor/main.c
@@ -6,7 +6,7 @@
 /*   By: roddavid <roddavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 14:41:40 by roddavid          #+#    #+#             */
-/*   Updated: 2019/04/11 16:58:07 by clrichar         ###   ########.fr       */
+/*   Updated: 2019/04/11 18:45:42 by clrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,27 @@ static bool				basic_err(void)
 		|| DEVENT != 4))
 		ret = 1;
 	return (ret == 0) ? true : false;
+}
+
+static void			print_man(void)
+{
+	ft_putendl("");
+	ft_putendl("\t\t*-------------------------------------------------------*");
+	ft_putendl("\t\t|                                                       |");
+	ft_putendl("\t\t|               Keybinding for the Editor               |");
+	ft_putendl("\t\t|                                                       |");
+	ft_putendl("\t\t|  Change panel: Tab                                    |");
+	ft_putendl("\t\t|  Select a Texture: 1 - 9 key                          |");
+	ft_putendl("\t\t|                                                       |");
+	ft_putendl("\t\t|  To save current map: 's' key                         |");
+	ft_putendl("\t\t|  To quit: 'echap' key                                 |");
+	ft_putendl("\t\t|                                                       |");
+	ft_putendl("\t\t|  Next Tier: Up Arrow       Previous Tier: Down Arrow  |");
+	ft_putendl("\t\t|  Add Y axis: '*' key       Remove Y axis: '/' key     |");
+	ft_putendl("\t\t|  Add X axis: '+' key       Remove X axis: '-' key     |");
+	ft_putendl("\t\t|                                                       |");
+	ft_putendl("\t\t*-------------------------------------------------------*");
+	ft_putendl("");
 }
 
 void				exit_error(int type, char *msg)
@@ -66,6 +87,7 @@ int		main(int ac, char **av)
 			parse_start(&e.data, av[1]);
 			init_from_parser(&e);
 		}
+		print_man();
 		main_loop(&e);
 	}
 	else
