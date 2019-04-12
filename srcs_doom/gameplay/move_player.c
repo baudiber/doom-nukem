afc046d74cf0a44d7657b421c0c81fc797581363
Gabriel GONZALEZ <gagonzal@e2r4p4.42.fr>
2019-04-12 22:49:43 +0200

    added max ceil for fly mode

--- srcs_doom/gameplay/move_player.c
+++ srcs_doom/gameplay/move_player.c
@@ -6,7 +6,7 @@
 /*   By: roddavid <roddavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 14:50:24 by roddavid          #+#    #+#             */
-/*   Updated: 2019/04/12 06:31:54 by gagonzal         ###   ########.fr       */
+/*   Updated: 2019/04/12 22:47:11 by gagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	fly_mode(t_env * e)
 		if (e->player.height > 10)
 			e->player.height -= 30;
 	}
-	else if (e->state[SDL_SCANCODE_SPACE])
+	else if (e->state[SDL_SCANCODE_SPACE] && e->player.height <= (MAX_FLOORS * 2) * TILE_SIZE)
 		e->player.height += 30;
 }
 
